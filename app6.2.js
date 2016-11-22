633044a88dafac540c60151c8107e3edaf7857ee
Nagaraju <nagaraju.kukudala@gmail.com>
2016-11-22 16:06:54 +0530

    Add files via upload

---
+++ app6.2.js
@@ -0,0 +1,14 @@
+//Greatest of 3 numbers
+var a= 10, b= 15, c=20;
+
+if(a>b && a> c)
+    {
+        console.log("Greatest Number among "+a+","+b+" and "+c+" is "+ a);
+    }
+else if(b>a && b>c)
+    {
+         console.log("Greatest Number among "+a+","+b+" and "+c+" is "+ b);
+    }
+else{
+         console.log("Greatest Number among "+a+","+b+" and "+c+" is "+ c);
+}
